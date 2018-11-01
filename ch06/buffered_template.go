3a54327a069a333df6f0505d5663749fce20c34f
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-01 18:08:26 +0100

    Add buffered template example

---
+++ ch06/buffered_template.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var t = template.Must(template.ParseFiles("simple.html"))
+
+type Page struct {
+	Title, Content string
+}
+
+func displayPage(w http.ResponseWriter, r *http.Request) {
+	p := &Page{
+		Title:   "An Example",
+		Content: "Have fun stormin' da castle.",
+	}
+
+	var b bytes.Buffer
+	err := t.Execute(w, p)
+	if err != nil {
+		fmt.Fprint(w, "An error occurred...")
+		return
+	}
+
+	b.WriteTo(w)
+}
+
+func main() {
+	http.HandleFunc("/", displayPage)
+	http.ListenAndServe(":8080", nil)
+}
